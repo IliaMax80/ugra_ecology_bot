35c6cd5e42926be25e6af4ae5cc2f6af8d074b4f
dsuction <da0434395@gmail.com>
2025-05-15 15:38:55 +0500

    Update utils.py

--- bot/utils/utils.py
+++ bot/utils/utils.py
@@ -142,28 +142,12 @@ async def find_closest(points, lat, lon):
 
 
 def to_camel(string: str) -> str:
-    """
-    Верблюдезирует строку, со строчным написанием первого слова
-
-    to_camel_case -> toCamelCase
-
-    :param string: строка в snake case'е
-    :type string: str
-    :return: строка в camel case'е
-    :rtype: str
-    """
     return ''.join(word if i == 0 else word.capitalize() for i, word in enumerate(string.split('_')))
 
 
 def to_snake(string: str) -> str:
-    """
-    Из CamelCase в snake_case
-    """
     return re.sub('(?!^)([A-Z]+)', r'_\1', string).lower()
 
 
 def to_cebab(string: str) -> str:
-    """
-    Из CamelCase в cebab-case
-    """
     return re.sub('(?!^)([A-Z]+)', r'-\1', string).lower()
