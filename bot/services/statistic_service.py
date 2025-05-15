49e38ed0224ffc193a04097a23dd430eb55bba04
dsuction <da0434395@gmail.com>
2025-05-15 15:39:52 +0500

    Update statistic_service.py

--- bot/services/statistic_service.py
+++ bot/services/statistic_service.py
@@ -12,9 +12,6 @@ class StatisticService:
 
     async def active_statistic(self, session: AsyncSession) -> ActiveStatistic:
         self.logger.debug('Fetching statistic for active users/commands')
-        # TODO: реализуй меня
-        # в ситуациях, когда какой-либо функционал не готов, его можно замокать и выдать другим разработчикам в
-        # команде интерфейс, на который они могут сразу же завязться и продолжить делать свою часть работы
         return ActiveStatistic(active_users=0, active_commands=0)
 
 
